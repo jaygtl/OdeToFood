863ac45a7a1a9263b2df6fb7b55c2f4f728ad1a0
Jay Rao <jay.rao@gatewaytechnolabs.com>
2019-08-04 18:12:47 +0200

    added datatable lib, logger and goThrough Startup

--- OdeToFood/OdeToFood/OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ OdeToFood/OdeToFood/OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using OdeToFood.Core;
 using OdeToFood.Data;
 
@@ -14,13 +15,16 @@ namespace OdeToFood.Pages.Restaurants
     {
         private readonly IConfiguration configuration;
         private readonly IRestaurantData restaurantData;
+        private readonly ILogger<ListModel> logger;
+
         public IEnumerable<Restaurant> Restaurants { get; set; }
         [BindProperty(SupportsGet =true)]
         public string SearchTerm { get; set; }
-        public ListModel(IConfiguration configuration, IRestaurantData restaurantData)
+        public ListModel(IConfiguration configuration, IRestaurantData restaurantData, ILogger<ListModel> logger)
         {
             this.configuration = configuration;
             this.restaurantData = restaurantData;
+            this.logger = logger;
         }
         public void OnGet()
         {
